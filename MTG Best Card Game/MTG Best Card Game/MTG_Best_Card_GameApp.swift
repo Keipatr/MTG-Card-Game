3d7eb2b395aff79c93dceeca554b06442f7fd3a5
hiu <95419527+Keipatr@users.noreply.github.com>
2023-11-10 10:23:47 +0700

    Add files via upload

---
+++ MTG Best Card Game/MTG Best Card Game/MTG_Best_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_Best_Card_GameApp.swift
+//  MTG Best Card Game
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MTG_Best_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
