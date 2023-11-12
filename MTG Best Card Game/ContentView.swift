06842cb48cebdba6ae8bd7cd0af86172b04d972c
Kei <kpatrick@student.ciputra.ac.id>
2023-11-13 02:53:05 +0700

    clicked

--- MTG Best Card Game/ContentView.swift
+++ MTG Best Card Game/ContentView.swift
@@ -25,7 +25,8 @@ struct Card: Codable {
     var oracleText: String?
     var colors: [String]?
     var prices: Prices?
-    var foil: Bool? // Add this line to include the foil property
+    var foil: Bool?
+    var legalities: Legalities?
 
     
     enum CodingKeys: String, CodingKey {
@@ -33,6 +34,7 @@ struct Card: Codable {
         case id
         case name
         case imageUris = "image_uris"
+        case legalities = "legalities"
         case manaCost = "mana_cost"
         case typeLine = "type_line"
         case oracleText = "oracle_text"
@@ -50,20 +52,75 @@ struct Prices: Codable {
     }
 }
 struct Legalities: Codable {
-    var small: String
-    var normal: String
-    var large:String
-    var png: String
-    var artCrop: String
-    var borderCrop: String
+    var standard: String
+    var future: String
+    var historic : String
+    var gladiator: String
+    var pioneer: String
+    var explorer: String
+    var modern: String
+    var legacy: String
+    var pauper: String
+    var vintage: String
+    var penny: String
+    var commander: String
+    var oathbreaker:String
+    var brawl: String
+    var historicbrawl: String
+    var alchemy: String
+    var paupercommander: String
+    var duel: String
+    var oldschool: String
+    var premodern: String
+    var predh: String
+    var allLegalities: [(format: String, legality: String)] {
+            return [
+                ("Standard", standard),
+                ("Future", future),
+                ("Historic", historic),
+                ("Gladiator", gladiator),
+                ("Pioneer", pioneer),
+                ("Explorer", explorer),
+                ("Modern", modern),
+                ("Legacy", legacy),
+                ("Pauper", pauper),
+                ("Vintage", vintage),
+                ("Penny", penny),
+                ("Commander", commander),
+                ("Oathbreaker", oathbreaker),
+                ("Brawl", brawl),
+                ("HistoricBrawl", historicbrawl),
+                ("Alchemy", alchemy),
+                ("PauperCommander", paupercommander),
+                ("Duel", duel),
+                ("OldSchool", oldschool),
+                ("Premodern", premodern),
+                ("Predh", predh)
+            ]
+        }
     
     enum CodingKeys: String, CodingKey {
-        case small
-        case normal
-        case large
-        case png
-        case artCrop = "art_crop"
-        case borderCrop = "border_crop"
+        case standard
+        case future
+        case historic
+        case gladiator
+        case pioneer
+        case explorer
+        case modern
+        case legacy
+        case pauper
+        case vintage
+        case penny
+        case commander
+        case oathbreaker
+        case brawl
+        case historicbrawl
+        case alchemy
+        case paupercommander
+        case duel
+        case oldschool
+        case premodern
+        case predh
     }
 }
 struct ImageUris: Codable {
@@ -138,7 +195,7 @@ struct ContentView: View {
                                     Text(card.name)
                                         .font(.caption)
                                         .lineLimit(1)
-                                        .frame(width: 100)
+                                        .frame(width: 100).foregroundColor(.black)
                                 }
                             }
                         }
@@ -193,42 +250,107 @@ struct CardImageView: View {
         }
     }
 }
-
-struct CardDetailView: View {
-    let card: Card
-
+struct LegalityView: View {
+    var legality: (status: String, format: String)
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                // Display the card image (you can reuse the CardImageView here)
-                CardImageView(card: card)
-                    .padding()
-                
-                Text(card.name)
-                    .font(.title)
-                    .padding()
-                
-                Text(card.typeLine)
-                    .font(.subheadline)
-                    .padding()
-                
-                Text(card.oracleText ?? "")
-                    .font(.body)
-                    .padding()
-                
-                // Display legalities (assuming you have a Legalities struct in your data)
-//                ForEach(card.legalities ?? [], id: \.format) { legality in
-//                    Text("\(legality.format): \(legality.legality)")
-//                        .font(.subheadline)
-//                        .padding()
-//                }
-                
-                Spacer()
-            }
+        HStack {
+            Text(legality.status)
+                .foregroundColor(.white)
+                .padding(.vertical, 4)
+                .padding(.horizontal, 8)
+                .background(legality.status == "legal" ? Color.green : Color.red)
+                .cornerRadius(5)
+            Text(legality.format)
+                .padding(.vertical, 4)
+                .frame(maxWidth: .infinity, alignment: .leading) // This ensures the text is left-aligned
         }
-        .navigationTitle("Card Details")
     }
 }
+
+struct CardLegalitiesView: View {
+    let legalities: [(status: String, format: String)]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, alignment: .leading, spacing: 10) {
+            ForEach(legalities, id: \.format) { legality in
+                LegalityView(legality: legality)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+let sampleLegalities = [
+        (status: "legal", format: "Legacy"),
+        (status: "not_legal", format: "Standard"),
+        // Add all other legalities here
+    ]
+struct CardDetailView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    let card: Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 0) { // Remove any default space between the views
+                // Image that fills the top part of the screen, including the safe area
+                if let artCropURL = card.imageUris?.artCrop {
+                    AsyncImage(url: artCropURL) { image in
+                        image.resizable()
+                             .aspectRatio(contentMode: .fill)
+                             .frame(width: geometry.size.width, height: geometry.safeAreaInsets.top + (geometry.size.width * 0.25)) // 1/4 of the width + safe area
+                             .clipped()
+                             .edgesIgnoringSafeArea(.top)
+                    } placeholder: {
+                        Color.gray
+                    }
+                }
+                // Card details
+                VStack(alignment: .leading) { // Reduced spacing and padding around text
+                    Text(card.name)
+                        .font(.system(size: 19))
+                        .fontWeight(.semibold)
+                    
+                    Text(card.typeLine)
+                        .font(.system(size: 18))
+                        .foregroundColor(.black).fontWeight(.semibold)
+                    
+                    if let oracleText = card.oracleText {
+                        Text(oracleText.replacingOccurrences(of: "\n", with: "\n\n"))
+                            .font(.system(size: 16))
+                    }
+                }
+                .padding([.horizontal, .bottom]) // Apply padding only to the sides and bottom
+                .background(Color.white)
+                .cornerRadius(10)
+                .offset(y: -geometry.safeAreaInsets.top) // Align the text block to the bottom edge of the image
+                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 6)
+                
+                CardLegalitiesView(legalities: sampleLegalities)
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+                .navigationBarBackButtonHidden(true) // Hide the default back button
+                    .navigationBarItems(leading: backButton)
+    }
+    var backButton: some View {
+           Button(action: {
+               self.presentationMode.wrappedValue.dismiss()
+           }) {
+               Image(systemName: "chevron.left") // Use chevron for "<" arrow shape
+                   .foregroundColor(.white)
+                   .imageScale(.large) // Increase the size if needed
+           }
+       }
+}
+
+
 #Preview {
     ContentView()
 }
