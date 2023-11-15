d55de8760a442970f5456d15c1762066627c7e3b
Kei <kpatrick@student.ciputra.ac.id>
2023-11-16 04:04:58 +0700

    finish

--- MTG Best Card Game/ContentView.swift
+++ MTG Best Card Game/ContentView.swift
@@ -27,7 +27,8 @@ struct Card: Codable {
     var prices: Prices?
     var foil: Bool?
     var legalities: Legalities?
-
+    var rank: Int
+    
     
     enum CodingKeys: String, CodingKey {
         case object
@@ -40,6 +41,8 @@ struct Card: Codable {
         case oracleText = "oracle_text"
         case colors
         case foil
+        case prices = "prices"
+        case rank = "edhrec_rank"
     }
 }
 struct Prices: Codable {
@@ -74,30 +77,30 @@ struct Legalities: Codable {
     var premodern: String
     var predh: String
     var allLegalities: [(format: String, legality: String)] {
-            return [
-                ("Standard", standard),
-                ("Future", future),
-                ("Historic", historic),
-                ("Gladiator", gladiator),
-                ("Pioneer", pioneer),
-                ("Explorer", explorer),
-                ("Modern", modern),
-                ("Legacy", legacy),
-                ("Pauper", pauper),
-                ("Vintage", vintage),
-                ("Penny", penny),
-                ("Commander", commander),
-                ("Oathbreaker", oathbreaker),
-                ("Brawl", brawl),
-                ("HistoricBrawl", historicbrawl),
-                ("Alchemy", alchemy),
-                ("PauperCommander", paupercommander),
-                ("Duel", duel),
-                ("OldSchool", oldschool),
-                ("Premodern", premodern),
-                ("Predh", predh)
-            ]
-        }
+        return [
+            ("Standard", standard),
+            ("Future", future),
+            ("Historic", historic),
+            ("Gladiator", gladiator),
+            ("Pioneer", pioneer),
+            ("Explorer", explorer),
+            ("Modern", modern),
+            ("Legacy", legacy),
+            ("Pauper", pauper),
+            ("Vintage", vintage),
+            ("Penny", penny),
+            ("Commander", commander),
+            ("Oathbreaker", oathbreaker),
+            ("Brawl", brawl),
+            ("Historic Brawl", historicbrawl),
+            ("Alchemy", alchemy),
+            ("Pauper Commander", paupercommander),
+            ("Duel", duel),
+            ("Old School", oldschool),
+            ("Premodern", premodern),
+            ("Predh", predh)
+        ]
+    }
     
     enum CodingKeys: String, CodingKey {
         case standard
@@ -141,12 +144,19 @@ struct ImageUris: Codable {
     }
 }
 
-// SwiftUI View that displays the cards
+enum SortingOption: String, CaseIterable {
+    case alphabetically = "Alphabetically"
+    case byRank = "By Rank"
+    case clear = "Clear Sorting"
+}
+
 struct ContentView: View {
     @State private var cards: [Card] = []
     @State private var searchText = ""
-    @State private var sortAlphabetically = false
-
+    @State private var isSortingOptionsSheetPresented = false
+    @State private var selectedSortingOption: SortingOption? = nil
+    @State private var currentIndex = 0
+    
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible()),
@@ -157,6 +167,7 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 HStack {
+                    
                     // Search bar
                     TextField("Search", text: $searchText)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -164,11 +175,27 @@ struct ContentView: View {
                     
                     // Sort button
                     Button(action: {
-                        sortAlphabetically.toggle()
+                        isSortingOptionsSheetPresented.toggle()
                     }) {
-                        Image(systemName: sortAlphabetically ? "arrow.up.arrow.down.circle" : "arrow.up.arrow.down.circle.fill")
+                        Image(systemName: "arrow.up.arrow.down.circle")
                     }
-                    .padding(.trailing)
+                    
+                    .actionSheet(isPresented: $isSortingOptionsSheetPresented) {
+                        ActionSheet(
+                            title: Text("Sort Options"),
+                            buttons: SortingOption.allCases.map { option in
+                                    .default(Text(option.rawValue)) {
+                                        if option == selectedSortingOption {
+                                            selectedSortingOption = nil
+                                        } else {
+                                            selectedSortingOption = option
+                                        }
+                                    }
+                            }
+                        )
+                    }
+                    Text(selectedSortingOption?.rawValue ?? "No Sorting").padding(.trailing,25)
+                    
                 }
                 
                 ScrollView {
@@ -181,20 +208,23 @@ struct ContentView: View {
                             }
                         }
                         
-                        if sortAlphabetically {
+                        switch selectedSortingOption {
+                        case .alphabetically?:
                             return filteredCards.sorted { $0.name < $1.name }
-                        } else {
+                        case .byRank?:
+                            return filteredCards.sorted { $0.rank < $1.rank }
+                        default:
                             return filteredCards
                         }
                     }
                     LazyVGrid(columns: columns, spacing: 20) {
                         ForEach(filteredAndSortedCards, id: \.id) { card in
-                            NavigationLink(destination: CardDetailView(card: card)) {
+                            NavigationLink(destination: CardDetailView(card: card, currentIndex: $currentIndex, cards: filteredAndSortedCards)) {
                                 VStack {
                                     CardImageView(card: card)
                                     Text(card.name)
                                         .font(.caption)
-                                        .lineLimit(1)
+                                        .lineLimit(1).multilineTextAlignment(.center)
                                         .frame(width: 100).foregroundColor(.black)
                                 }
                             }
@@ -202,14 +232,18 @@ struct ContentView: View {
                     }
                     .padding()
                 }
-                .navigationTitle("Cards")
+                .navigationTitle("Magic The Gathering")
+                .toolbarColorScheme(.dark, for: .navigationBar)
+                .navigationBarTitleDisplayMode(.inline).toolbarBackground(
+                    Color(red: 44 / 255, green: 61 / 255, blue: 81 / 255),
+                    for: .navigationBar)
+                .toolbarBackground(.visible, for: .navigationBar)
             }
             .onAppear(perform: loadCards)
         }
     }
     // Function to load and parse the JSON data
     func loadCards() {
-        // Replace this with the actual path to your JSON file in the app bundle
         if let url = Bundle.main.url(forResource: "WOT-Scryfall", withExtension: "json"),
            let data = try? Data(contentsOf: url) {
             let decoder = JSONDecoder()
@@ -222,9 +256,10 @@ struct ContentView: View {
         }
     }
 }
+
 struct CardImageView: View {
     let card: Card
-
+    
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
             if let imageUris = card.imageUris {
@@ -235,120 +270,186 @@ struct CardImageView: View {
                 }
                 .frame(width: 100, height: 140)
                 .cornerRadius(5)
-
-                if card.foil == true{
-                    Text("F")
-                        .font(.caption)
-                        .fontWeight(.bold)
-                        .padding(4)
-                        .background(Color.yellow.opacity(0.7))
-                        .foregroundColor(.white)
-                        .cornerRadius(3)
-                        .padding([.bottom, .leading], 4)
+                
+                HStack(spacing: 4) {
+                    if card.foil == true {
+                        Text("F")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(4)
+                            .background(Color.yellow)
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                    }
+                    if let price = card.prices?.usdFoil ?? card.prices?.usd {
+                        Text("$\(price)")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
                 }
+                .padding(4)
+                .background(Color.black.opacity(0.6))
+                .cornerRadius(9)
+                .padding([.bottom, .leading], 4)
             }
-        }
-    }
-}
-struct LegalityView: View {
-    var legality: (status: String, format: String)
-    
-    var body: some View {
-        HStack {
-            Text(legality.status)
-                .foregroundColor(.white)
-                .padding(.vertical, 4)
-                .padding(.horizontal, 8)
-                .background(legality.status == "legal" ? Color.green : Color.red)
-                .cornerRadius(5)
-            Text(legality.format)
-                .padding(.vertical, 4)
-                .frame(maxWidth: .infinity, alignment: .leading) // This ensures the text is left-aligned
         }
     }
 }
 
-struct CardLegalitiesView: View {
-    let legalities: [(status: String, format: String)]
-    
-    let columns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-    
-    var body: some View {
-        LazyVGrid(columns: columns, alignment: .leading, spacing: 10) {
-            ForEach(legalities, id: \.format) { legality in
-                LegalityView(legality: legality)
-            }
-        }
-        .padding(.horizontal)
-    }
-}
-let sampleLegalities = [
-        (status: "legal", format: "Legacy"),
-        (status: "not_legal", format: "Standard"),
-        // Add all other legalities here
-    ]
 struct CardDetailView: View {
     @Environment(\.presentationMode) var presentationMode
-
-    let card: Card
     
+    let card: Card
+    @Binding var currentIndex: Int
+    let cards: [Card]
     var body: some View {
+        let card = cards[currentIndex]
         GeometryReader { geometry in
-            VStack(spacing: 0) { // Remove any default space between the views
-                // Image that fills the top part of the screen, including the safe area
-                if let artCropURL = card.imageUris?.artCrop {
-                    AsyncImage(url: artCropURL) { image in
-                        image.resizable()
-                             .aspectRatio(contentMode: .fill)
-                             .frame(width: geometry.size.width, height: geometry.safeAreaInsets.top + (geometry.size.width * 0.25)) // 1/4 of the width + safe area
-                             .clipped()
-                             .edgesIgnoringSafeArea(.top)
-                    } placeholder: {
-                        Color.gray
-                    }
-                }
-                // Card details
-                VStack(alignment: .leading) { // Reduced spacing and padding around text
-                    Text(card.name)
-                        .font(.system(size: 19))
-                        .fontWeight(.semibold)
+            ScrollView{
+                VStack(spacing: 10)
+                {
+                    VStack(alignment: .leading) {
+                        if let artCropURL = card.imageUris?.artCrop {
+                            AsyncImage(url: artCropURL) { image in
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: geometry.size.width, height: geometry.safeAreaInsets.top + (geometry.size.width * 0.6))
+                                    .clipped()
+                                    .edgesIgnoringSafeArea(.top)
+                            } placeholder: {
+                                Color.gray
+                            }.edgesIgnoringSafeArea(.top)
+                        }
+                        Text(card.name)
+                            .font(.system(size: 19))
+                            .fontWeight(.semibold).padding(.horizontal,10).padding(.top,10)
+                        
+                        Text(card.typeLine)
+                            .font(.system(size: 18))
+                            .foregroundColor(.black)
+                            .fontWeight(.semibold).padding(.horizontal, 10)
+                        
+                        if let oracleText = card.oracleText {
+                            Text(oracleText.replacingOccurrences(of: "\n", with: "\n\n"))
+                                .font(.system(size: 16)).padding(.horizontal, 10).padding(.bottom,14)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }
+                    }.frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .offset(y: -geometry.safeAreaInsets.top)
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 6)
                     
-                    Text(card.typeLine)
-                        .font(.system(size: 18))
-                        .foregroundColor(.black).fontWeight(.semibold)
-                    
-                    if let oracleText = card.oracleText {
-                        Text(oracleText.replacingOccurrences(of: "\n", with: "\n\n"))
-                            .font(.system(size: 16))
+                    // Legalities section
+                    VStack(alignment: .leading) {
+                        HStack {
+                            if currentIndex > 0 {
+                                Button(action: swipeRight) {
+                                    Image(systemName: "arrow.left.circle")
+                                        .imageScale(.large)
+                                }
+                            } else {
+                                Image(systemName: "arrow.left.circle")
+                                    .imageScale(.large)
+                                    .opacity(0)
+                            }
+                            
+                            Text("Legalities")
+                                .font(.headline)
+                            
+                            if currentIndex < cards.count - 1 {
+                                Button(action: swipeLeft) {
+                                    Image(systemName: "arrow.right.circle")
+                                        .imageScale(.large)
+                                }
+                            } else {
+                                Image(systemName: "arrow.right.circle")
+                                    .imageScale(.large)
+                                    .opacity(0)
+                            }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        if let legalities = card.legalities {
+                            let gameTypes = legalities.allLegalities
+                            if !gameTypes.isEmpty {
+                                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
+                                    ForEach(gameTypes, id: \.format) { gameType in
+                                        let isLegal = gameType.legality == "legal"
+                                        let boxColor = isLegal ? Color.green : Color.gray
+                                        let text = isLegal ? "Legal" : "Not Legal"
+                                        
+                                        HStack {
+                                            Text(text).font(.system(size: 14)).frame(width: 65)
+                                                .padding(10)
+                                                .background(RoundedRectangle(cornerRadius: 10).fill(boxColor))
+                                                .foregroundColor(.black)
+                                                .frame(maxWidth: .infinity, minHeight: 40)
+                                                .alignmentGuide(.leading) { _ in 0 }
+                                            
+                                            Text(gameType.format)
+                                                .font(.system(size: 14))
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                        }.padding(.horizontal,8)
+                                            .frame(maxWidth: .infinity)
+                                            .padding(.vertical, 5)
+                                    }
+                                }
+                            }
+                            
+                        }
                     }
+                    .offset(y: -geometry.safeAreaInsets.top)
                 }
-                .padding([.horizontal, .bottom]) // Apply padding only to the sides and bottom
-                .background(Color.white)
-                .cornerRadius(10)
-                .offset(y: -geometry.safeAreaInsets.top) // Align the text block to the bottom edge of the image
-                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 6)
-                
-                CardLegalitiesView(legalities: sampleLegalities)
             }
             
+            .gesture(DragGesture().onEnded(handleSwipe))
+            .edgesIgnoringSafeArea(.top)
+            
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: backButton)
+            
         }
-        .edgesIgnoringSafeArea(.bottom)
-                .navigationBarBackButtonHidden(true) // Hide the default back button
-                    .navigationBarItems(leading: backButton)
+        
+    }
+    
+    private func handleSwipe(_ value: DragGesture.Value) {
+        let swipeDistance = value.translation.width
+        
+        if swipeDistance < -50 {
+            swipeLeft()
+        } else if swipeDistance > 50 {
+            swipeRight()
+        }
+    }
+    
+    private func swipeLeft() {
+        if currentIndex < cards.count - 1 {
+            currentIndex += 1
+            print("Moved to next card: Index \(currentIndex)")
+        } else {
+            print("No more cards on the left")
+        }
+    }
+    
+    private func swipeRight() {
+        if currentIndex > 0 {
+            currentIndex -= 1
+            print("Moved to previous card: Index \(currentIndex)")
+        } else {
+            print("No more cards on the right")
+        }
     }
     var backButton: some View {
-           Button(action: {
-               self.presentationMode.wrappedValue.dismiss()
-           }) {
-               Image(systemName: "chevron.left") // Use chevron for "<" arrow shape
-                   .foregroundColor(.white)
-                   .imageScale(.large) // Increase the size if needed
-           }
-       }
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.black)
+                .imageScale(.large)
+        }
+    }
 }
+
 
 
 #Preview {
